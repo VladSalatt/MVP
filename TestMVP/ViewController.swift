eb2929abcfb5efadbbab574232acaf4c1aa0cc93
Vladislav Koshelev <VKoshelev@detmir.ru>
2021-09-02 10:42:23 +0300

    Initial Commit

---
+++ TestMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMVP
+//
+//  Created by VKoshelev@detmir.ru on 02.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
